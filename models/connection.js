02a26101373945061a51cedf620459902f2bda82
Juan Formoso <trajano.juan@gmail.com>
2022-01-28 15:38:58 -0300

    Add StoreManager databse in connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD } = process.env;
 
 const connection = mysql.createPool({
   host: MYSQL_HOST,
   user: MYSQL_USER,
   password: MYSQL_PASSWORD,
-  database: MYSQL_DATABASE,
+  database: 'StoreManager',
 });
 
 module.exports = connection;
