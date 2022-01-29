c0e381168512d4e4a2b46e0afcf15290d303081b
Juan Formoso <trajano.juan@gmail.com>
2022-01-29 20:44:53 -0300

    Recomeçando do zero

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-const productsRouter = require('./routes/products');
-const errorMiddleware = require('./middlewares/error');
-
-app.use('/products', productsRouter);
-app.use(errorMiddleware);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
 });
--- routers/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const productsController = require('../controllers/products');
-
-router.post('/', productsController.createProduct);
-
-module.exports = router;
