3e0d901699dd5ae75f4e595cb9d606dbaac03d37
Juan Formoso <trajano.juan@gmail.com>
2022-01-31 11:32:59 -0300

    Recomeçando projeto

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const productsController = require('./controllers/products');
-
-app.use(bodyParser.json());
-
-require('dotenv').config();
-
-app.use('/products', productsController);
-
-app.use((err, req, res, next) => {
-  if (err.code) {
-    return res
-      .status(err.code)
-      .json({ status: err.status, message: err.message });
-  }
-  return next(err);
-});
-
-app.use((err, req, res, next) => {
-  const errorMap = {
-    notFound: 404,
-  };
-  const status = errorMap[err.code];
-  if (!status) {
-    next(err);
-  }
-  return res.status(status).json(err);
-});
-
-app.use((err, req, res, _next) =>
-  res.status(500).json({
-    code: 'internal_server_error',
-    message: 'error processing request',
-  }));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
