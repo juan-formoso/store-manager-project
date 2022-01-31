96acf45c6502cc78bf63267c8a02b3d7aa28092d
Juan Formoso <trajano.juan@gmail.com>
2022-01-31 20:30:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { router } = require('./routers/products.routes');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use('/products', router);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
