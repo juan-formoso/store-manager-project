6656ca07e3edebe9ef63a4dedd1b02beaad14d78
Juan Formoso <trajano.juan@gmail.com>
2022-02-03 16:19:38 -0300

    Corrigindo erro de verificação

--- controllers/sales.js
+++ controllers/sales.js
@@ -70,10 +70,11 @@ const productIdValidation = async (req, res, next) => {
 const quantityValidation = async (req, res, next) => {
   const sales = req.body;
   const quantity = sales.every((sale) => (Object.keys(sale).includes('quantity')));
+  const quantityIsNumber = sales.every((sale) => (typeof sale.quantity === 'number'));
   if (!quantity && quantity !== 0) {
     return res.status(400).json({ message: '"quantity" is required' });
   }
-  if (typeof quantity !== 'number' || quantity < 1) {
+  if (typeof quantityIsNumber !== 'number' || quantityIsNumber < 1) {
     return res.status(422).json({ 
       message: '"quantity" must be a number larger than or equal to 1',
     });
