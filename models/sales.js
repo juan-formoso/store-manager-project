fffdcfdf9559368b28defb3642364da72734779a
Juan Formoso <trajano.juan@gmail.com>
2022-02-03 10:49:59 -0300

    Refatorado sales models

--- models/sales.js
+++ models/sales.js
@@ -1,27 +1,32 @@
 const connection = require('./connection');
 
-const salesValues = (sales, id) => {
-  let values = '';
-  sales.forEach(({ product_id: productId, quantity }, i) => {
-    if (i) {
-      values += `, (${id}, ${productId}, ${quantity})`;
-    } else values += `(${id}, ${productId}, ${quantity})`;
-  });
-  return values;
+const insertSale = async () => {
+  const [rows] = await connection.execute('INSERT INTO StoreManager.sales VALUE ()');
+  return rows.insertId;
 };
 
-const insertSale = async (sales) => {
-  const [rows] = await connection.execute('INSERT INTO sales VALUES ()');
-  await connection.execute(
-    `INSERT INTO sales_products (sale_id, product_id, quantity) VALUES ${salesValues(
-      sales,
-      rows.insertId,
-    )}`,
+const create = async (sales) => {
+  const id = await insertSale();
+  const query = 'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUE ?';
+  const saleValues = sales.map((sale) => [id, sale.product_id, sale.quantity]);
+  await connection.query(
+    query, [saleValues],
   );
   return {
-    id: rows.insertId,
+    id,
     itemsSold: sales,
   };
 };
 
-module.exports = { insertSale };
+const getSales = async () => {
+  const query = `SELECT p.sale_id AS saleId, s.date, p.product_id, p.quantity
+                FROM
+                    StoreManager.sales_products AS p
+                INNER JOIN
+                    StoreManager.sales AS s
+                ON p.sale_id = s.id;`;
+  const [rows] = await connection.execute(query);
+  return rows;
+};
+
+module.exports = { create, getSales };
