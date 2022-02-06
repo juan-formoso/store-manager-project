59416139175b6cfafa63eab49d92f7ba78904cf4
Juan Formoso <trajano.juan@gmail.com>
2022-02-05 21:44:30 -0300

    Refatorando product models e add novas funções

--- models/products.js
+++ models/products.js
@@ -1,41 +1,119 @@
 const connection = require('./connection');
 
+const errorMessage = 'Internal Server Error';
+
+const insertProduct = async ({ name, quantity }) => {
+  try {
+    const [rows] = await connection.execute(
+      'INSERT INTO StoreManager.products (`name`, `quantity`) VALUE (?, ?);', 
+      [name, quantity],
+    );
+    return {
+      id: rows.insertId,
+      name,
+      quantity,
+    };
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
+};
+
 const getByName = async (name) => {
-  const [rows] = await connection.execute('SELECT * FROM products WHERE name = ?', [name]);
-  return rows[0];
+  try {
+    const [rows] = await connection.execute(
+      'SELECT name FROM StoreManager.products WHERE name = ?;', [name],
+    );
+    return rows;
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
 };
 
-const insertProduct = async (name, quantity) => {
-  const [rows] = await connection.execute(
-    'INSERT INTO products (name, quantity) VALUES (?, ?)',
-    [name, quantity],
-  );
-  return {
-    id: rows.insertId,
-    name,
-    quantity,
-  };
-};
-
-const getProducts = async () => {
-  const [rows] = await connection.execute('SELECT * FROM products');
-  return rows;
+const getAll = async () => {
+  try {
+    const [rows] = await connection.execute('SELECT * FROM StoreManager.products;');
+    return rows;
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
 };
 
 const getById = async (id) => {
-  const [rows] = await connection.execute('SELECT * FROM products WHERE id = ?', [id]);
-  return rows[0];
+  try {
+    const [rows] = await connection.execute(
+      'SELECT * FROM StoreManager.products WHERE id = ?;', 
+      [id],
+    );
+    return rows;
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
 };
 
-const update = async ({ id, name, quantity }) => {
-  await connection.execute(
-    'UPDATE products SET name = ?, quantity = ? WHERE id = ?', [name, quantity, id],
-  );
-  return { id, name, quantity };
+const updateProduct = async ({ id, name, quantity }) => {
+  try {
+    const [newProduct] = await connection.execute(
+      'UPDATE StoreManager.products SET name = ?, quantity = ? WHERE id = ?;', 
+      [id, name, quantity],
+    );
+    return {
+      id: newProduct.insertId,
+      name,
+      quantity,
+    };
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
 };
 
-const remove = async (id) => {
-  await connection.execute('DELETE FROM products WHERE id = ?', [id]);
+const deleteById = async (id) => {
+  try {
+    const [rows] = await connection.execute(
+      'DELETE FROM StoreManager.products WHERE id = ?;', [id],
+    );
+    return {
+      id: rows.insertId,
+    };
+  } catch (error) {
+    console.log(`Model ${error}`);
+    return {
+      error: true,
+      codeStatus: 500,
+      message: errorMessage,
+    };
+  }
 };
 
-module.exports = { insertProduct, getByName, getProducts, getById, update, remove };
+module.exports = {
+  insertProduct,
+  getByName,
+  getAll,
+  getById,
+  updateProduct,
+  deleteById,
+};
