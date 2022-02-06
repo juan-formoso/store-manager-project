63728ad5d53f92858cb150c4a891f6473c93f2ae
Juan Formoso <trajano.juan@gmail.com>
2022-02-05 21:44:40 -0300

    Refatorando sales models e add novas funções

--- models/sales.js
+++ models/sales.js
@@ -1,41 +1,145 @@
 const connection = require('./connection');
 
-const insertNewId = async () => {
-  const [rows] = await connection.execute('INSERT INTO StoreManager.sales VALUES ()');
-  return rows.insertId;
+const errorMessage = 'Internal Server Error';
+
+const saleID = async () => {
+  try {
+    const [rows] = await connection.execute(
+      'SELECT MAX(`id`) AS `id` FROM StoreManager.sales;',
+    );
+    const id = rows[0].id + 1 || 1;
+    return id;
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
 };
 
-const insertSale = async (sales) => {
-  const id = await insertNewId();
-  const query = 'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES ?';
-  const saleValue = sales.map((sale) => [id, sale.product_id, sale.quantity]);
-  await connection.query(query, [saleValue]);
-  return { id, itemsSold: sales };
+const newSale = async () => {
+  try {
+    const id = await saleID();
+    await connection.execute(
+      'INSERT INTO StoreManager.sales (id) VALUE (?);',
+      [id],
+    );
+    return id;
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
 };
 
-/* const getSales = async () => {
-  const [rows] = await connection.execute(
-    `SELECT s.id AS saleId, s.date AS date, p.id AS product_id, sp.quantity AS quantity
-    FROM StoreManager.sales_products AS sp
-    INNER JOIN StoreManager.products AS p
-    ON sp.product_id = p.id
-    INNER JOIN StoreManager.sales AS s
-    ON sp.sale_id = s.id;`,
-  );
-  return rows;
+const insertSale = async (saleId, sales) => {
+  try {
+    const salesArr = sales.map(({ product_id: productId, quantity }) => (
+      [saleId, productId, quantity]
+    ));
+    await connection.query(
+      'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUE ?',
+      [salesArr],
+    );
+    return {
+      id: saleId,
+      itemsSold: sales,
+    };
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+const updateQuantityCreate = async ({ product_id: productId, quantity }) => {
+  try {
+    await connection.execute(
+      `UPDATE StoreManager.products SET quantity = quantity - ?
+      WHERE id = ?;`, [quantity, productId],
+    );
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+const getAll = async () => {
+  try {
+    const [rows] = await connection.execute(
+      `SELECT P.sale_id AS saleId, S.date, P.product_id, P.quantity
+      FROM StoreManager.sales_products AS P
+      INNER JOIN StoreManager.sales AS S ON P.sale_id = S.id;`,
+    );
+    return rows;
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
 };
 
 const getById = async (id) => {
-  const [rows] = await connection.execute(
-    `SELECT s.id AS sale_id, s.date AS date, p.id AS product_id, sp.quantity AS quantity
-    FROM StoreManager.sales_products AS sp
-    INNER JOIN StoreManager.products AS p
-    ON sp.product_id = p.id
-    INNER JOIN StoreManager.sales AS s
-    ON sp.sale_id = s.id
-    WHERE s.id = ?`, [id],
-  );
-  return rows;
-}; */
+  try {
+    const [rows] = await connection.execute(
+      `SELECT S.date, P.product_id, P.quantity FROM StoreManager.sales_products AS P
+      INNER JOIN StoreManager.sales AS S ON P.sale_id = S.id WHERE P.sale_id = ?;`,
+      [id],
+    );
+    return rows;
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
 
-module.exports = { insertSale/* , getSales, getById */ };
+const updateSale = async (id, sale) => {
+  try {
+    const { product_id: productId, quantity } = sale[0];
+    await connection.execute(
+      `UPDATE StoreManager.sales_products SET product_id = ?, quantity = ?
+      WHERE sale_id = ?;`, [productId, quantity, id],
+    );
+    return {
+      saleId: id,
+      itemUpdated: sale,
+    };
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+const deleteById = async (id) => {
+  try {
+    const [rows] = await connection.execute(
+      'DELETE FROM StoreManager.sales_products WHERE sale_id = ?', [id],
+    );
+    return rows;
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+const updateQuantityDelete = async (quantity, productId) => {
+  try {
+    await connection.execute(
+      `UPDATE StoreManager.products SET quantity = quantity + ?
+      WHERE id = ?;`, [quantity, productId],
+    );
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+const getProductQuantity = async (productId) => {
+  try {
+    const [rows] = await connection.execute(
+      'SELECT quantity FROM StoreManager.products WHERE id = ?', [productId],
+    );
+    return rows[0];
+  } catch (error) {
+    return { error: true, codeStatus: 500, message: errorMessage };
+  }
+};
+
+module.exports = {
+  saleID,
+  newSale,
+  insertSale,
+  updateQuantityCreate,
+  getAll,
+  getById,
+  updateSale,
+  deleteById,
+  updateQuantityDelete,
+  getProductQuantity,
+};
