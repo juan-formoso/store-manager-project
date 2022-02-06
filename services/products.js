2cce7efa914f3739154eec1aba560f4f29d20cc3
Juan Formoso <trajano.juan@gmail.com>
2022-02-05 22:19:04 -0300

    Update product services

--- services/products.js
+++ services/products.js
@@ -1,5 +1,67 @@
 const model = require('../models/products');
 
-const getByName = async (name) => model.getByName(name);
+const insertProduct = async ({ name, quantity }) => {
+  try {
+    const response = await model.insertProduct({
+      name,
+      quantity,
+    });  
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
 
-module.exports(getByName);
+const getByName = async (name) => {
+  try {
+    const response = await model.getByName(name);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getAll = async () => {
+  try {
+    const response = await model.getAll();
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getById = async (id) => {
+  try {
+    const response = await model.getById(id);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateProduct = async ({ id, name, quantity }) => {
+  try {
+    const response = await model.updateProduct({ id, name, quantity });
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteById = async (id) => {
+  try {
+    const response = await model.deleteById(id);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  insertProduct,
+  getByName,
+  getAll,
+  getById,
+  updateProduct,
+  deleteById,
+};
