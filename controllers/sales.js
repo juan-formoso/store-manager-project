4a2db87d5aa54b3d429362d8d792b173cd781854
Juan Formoso <trajano.juan@gmail.com>
2022-02-06 01:26:14 -0300

    Refatorando sales controller e add update e delete

--- controllers/sales.js
+++ controllers/sales.js
@@ -1,53 +1,67 @@
-const { insertSale/* , getSales, getById */ } = require('../models/sales');
-
-const productIdValidation = async (req, res, next) => {
-  const sales = req.body;
-  const productId = sales.every((sale) => (Object.keys(sale).includes('product_id')));
-  if (!productId) return res.status(400).json({ message: '"product_id" is required' });
-  next();
-};
-
-const quantityValidation = async (req, res, next) => {
-  const sales = req.body;
-  const quantity = sales.every((sale) => (Object.keys(sale).includes('quantity')));
-  const quantityIsNumber = sales.every((sale) => (
-    typeof sale.quantity === 'number' && sale.quantity > 0
-  ));
-  if (!quantity) {
-    return res.status(400).json({ message: '"quantity" is required' });
-  }
-  if (!quantityIsNumber) {
-    return res.status(422).json({ 
-      message: '"quantity" must be a number larger than or equal to 1',
-    });
-  }
-  next();
-};
+const services = require('../services/sales');
 
 const createSale = async (req, res) => {
-  const sales = req.body;
-  const newSale = await insertSale(sales);
-  return res.status(201).json(newSale);
-};
-
-/* const getSaleById = async (req, res) => {
-  const { params } = req;
-  const getProduct = await getById(params.id);
-  if (!getProduct.length) { 
-    return res.status(404).json({ message: 'Sale not found' }); 
+  try {
+    const saleId = await services.newSale();
+    const { body } = req;
+    const response = await services.insertSale(saleId, body);
+    body.forEach(async (b) => {
+      await services.deleteQuantity(b);
+    });
+    return res.status(201).json(response);
+  } catch (error) {
+    return error;
   }
-  return res.json(getProduct);
 };
 
-const getAllSales = async (req, res) => {
-  const sales = await getSales();
-  res.json(sales);
-}; */
+const getAll = async (_req, res) => {
+  try {
+    const response = await services.getAll();
+    return res.status(200).json(response);
+  } catch (error) {
+    return error;
+  }
+};
 
-module.exports = { 
-  createSale, 
-  productIdValidation, 
-  quantityValidation,
-  /* getAllSales, 
-  getSaleById, */
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await services.getById(Number(id));
+    if (!response.length) return res.status(404).json({ message: 'Sale not found' });
+    return res.status(200).json(response);
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateSale = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const response = await services.updateSale(Number(id), body);
+    return res.status(200).json(response);
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const saleId = await services.getById(Number(id));
+    if (!saleId.length) res.status(404).json({ message: 'Sale not found' });
+    await services.updateQuantity(saleId[0].quantity, saleId[0].product_id);
+    await services.deleteById(Number(id));
+    return res.status(200).json(saleId);
+  } catch (error) {
+    return error;
+  }
+};
+
+module.exports = {
+  createSale,
+  getAll,
+  getById,
+  updateSale,
+  deleteById,
 };
