9126acd3b65cd85380883d5edc4761a3f4e5e41a
Juan Formoso <trajano.juan@gmail.com>
2022-02-06 22:08:36 -0300

    Update sales.routes.js

--- routers/sales.routes.js
+++ routers/sales.routes.js
@@ -1,15 +1,57 @@
 const salesRouter = require('express').Router();
+
 const { 
-  productIdValidation, 
-  quantityValidation,
   createSale,
-  /* saleNotFound,
-  getAllSales,
-  getSaleById, */
+  getAll,
+  getById,
+  updateSale,
+  deleteById,
 } = require('../controllers/sales');
 
-salesRouter.post('/', productIdValidation, quantityValidation, createSale);
-salesRouter.get('/'/* , getAllSales */);
-salesRouter.get('/id'/* , saleNotFound, getSaleById */);
+const { 
+  productIdValidation,
+  salesQuantityValidation,
+  quantityIsNumber,
+  amountValidation,
+} = require('../helpers/sales');
+
+salesRouter.post(
+  '/', 
+  productIdValidation, 
+  salesQuantityValidation, 
+  quantityIsNumber, 
+  amountValidation, 
+  createSale,
+);
+
+salesRouter.get('/', getAll);
+
+salesRouter.get('/:id', getById);
+
+salesRouter.put('/:id', productIdValidation, salesQuantityValidation, quantityIsNumber, updateSale);
+
+salesRouter.delete('/:id', deleteById);
+
+/* app.route('/sales')
+  .post(
+    productIdExists,
+    quantitySalesExists,
+    validQuantityIsNumber,
+    checkQuantity,
+    salesController.create,
+  );
+
+app.route('/sales')
+  .get(salesController.getAll);
+
+app.route('/sales/:id')
+  .get(salesController.getById)
+  .put(
+    productIdExists,
+    quantitySalesExists,
+    validQuantityIsNumber,
+    salesController.update,
+  )
+  .delete(salesController.deleteById); */
 
 module.exports = salesRouter;
